ed69925440003d77c1327ca3b7c14ced2fdc4e3b
Mehdi Hassan Akash <mhakash211998@gmail.com>
2022-05-07 13:07:58 +0600

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mehid Hassan Akash</title>
+        <meta name="description" content="Mehid Hassan Akash" />
+        <link rel="icon" href="/icon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,26 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Mehid Hassan Akash</title>
-        <meta name="description" content="Mehid Hassan Akash" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
-        Hello world
+        <h1 className="text-xl">Hello world</h1>
       </main>
 
       <footer className={styles.footer}>
-        
+        <Image
+          src="/icon.svg"
+          alt="mhakash logo"
+          className={styles.logo}
+          width={20}
+          height={20}
+        />
+        <span className="ml-2">2022</span>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
