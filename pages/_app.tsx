09159e3cc214aac7141cfb472edeaf33dad8a7a1
Mehdi Hassan Akash <mhakash211998@gmail.com>
2022-12-04 11:50:26 +0600

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Mehid Hassan Akash</title>
-        <meta name="description" content="Mehid Hassan Akash" />
+        <title>Mehdi Hassan Akash</title>
+        <meta name="description" content="Mehdi Hassan Akash" />
         <link rel="icon" href="/icon.svg" />
       </Head>
       <Component {...pageProps} />
