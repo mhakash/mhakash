63fa0508cb28fd624ce4d2104c5c2c4294d5e5c6
Mehdi Hassan Akash <mhakash211998@gmail.com>
2023-03-25 20:27:24 +0600

    update info

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Profile from "./profile.mdx";
 
 export default function HomePage() {
   return (
-    <div className="prose prose-gray py-8">
+    <div className="prose prose-gray py-8 px-4 md:px-0">
       <Profile />
     </div>
   );
