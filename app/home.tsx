4c38799f15e1a427d9be78d524d451c809b766cf
Mehdi Hassan Akash <mhakash211998@gmail.com>
2026-03-26 23:53:34 +0600

    update website

--- app/home.tsx
+++ app/home.tsx
@@ -1,13 +1,26 @@
 'use client';
 
+import Image from 'next/image';
 import HomeContent from '@/data/markdown/home.mdx';
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <article className="p-4 mx-auto prose">
-        <HomeContent />
-      </article>
-    </div>
+    <>
+      <div className="fixed top-0 left-0 right-0 h-0.5 bg-teal-700 z-50" />
+      <div className="w-full py-16 px-4 pt-18">
+        <div className="mx-auto max-w-prose">
+          <Image
+            src="/mhakash.svg"
+            alt="mhakash logo"
+            width={52}
+            height={52}
+            className="mb-10 opacity-85"
+          />
+          <article className="prose prose-stone prose-a:text-teal-700 prose-a:decoration-teal-700/40 prose-a:underline-offset-2 prose-hr:border-stone-200">
+            <HomeContent />
+          </article>
+        </div>
+      </div>
+    </>
   );
 }
